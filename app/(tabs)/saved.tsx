372a18f1fe3f699b9475db9aafc76557a6245354
David Offei Akuffo <david.akuffo11@gmail.com>
2025-07-31 22:45:46 +0000

    mobile app

---
+++ app/(tabs)/saved.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const saved = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default saved;
+
+const styles = StyleSheet.create({});
